08246f34ac4e854b9fdfaf077195230cf66af80f
Dipesh Vyas <dipesh@protocolzone.com>
2022-03-04 10:07:55 +0530

    First_Project

---
+++ src/main/java/gradle_project/GitPractice.java
@@ -0,0 +1,15 @@
+package gradle_project;
+
+public class GitPractice {
+
+	public static void main(String[] args){
+
+		final String s=" ";
+		final String s1="Akshay kishor sonawane";
+		
+		System.out.println(s.isBlank());
+		System.out.println(s1.charAt(2));
+
+	}
+
+}
